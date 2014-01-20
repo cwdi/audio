414167413184b8353298dfcc06dc48460b3a1534
cwdi <cwalker@dimin.com>
2014-01-20 14:37:22 -0600

    Update buttons2.py

--- buttons2.py
+++ buttons2.py
@@ -14,14 +14,8 @@ good = ['youreright.wav','Cheering.wav']
 def main():
     """ Set up the game and run the main game loop """
     pygame.init()      # Prepare the pygame module for use
-    surface_sz = 1050   # Desired physical surface size, in pixels.
 
-    # Create surface of (width, height), and its window.
-    main_surface = pygame.display.set_mode((1680, surface_sz))
 
-    # Set up some data to describe a small rectangle and its color
-    small_rect = (300, 200, 150, 90)
-    some_color = (255, 0, 0)        # A color is a mix of (Red, Green, Blue)
 
     def wrongPress(y):
         pygame.mixer.music.load(y)
@@ -58,15 +52,6 @@ def main():
 
         # Update your game objects and data structures here...
 
-        # We draw everything from scratch on each frame.
-        # So first fill everything with the background color
-        main_surface.fill((0, 200, 255))
-
-        # Overpaint a smaller rectangle on the main surface
-        main_surface.fill(some_color, small_rect)
-
-        # Now the surface is ready, tell pygame to display it!
-        pygame.display.flip()
 
     pygame.quit()     # Once we leave the loop, close the window.
 
